2367879e5cf6f53a7d970b11e5baa27c1a32288c
Mudassir Fayaz <105848832+Fayaz-khani@users.noreply.github.com>
2024-05-05 14:58:53 +0500

    Update about.py
    Updated the About section of Drsunny.ai

--- LlamaIndex/Streamlit/about.py
+++ LlamaIndex/Streamlit/about.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 def app():
-    st.subheader('OFIN is an application created for users to')
-    # st.subheader('To help quickly analyze their Businesses financial Condition')
-    st.subheader('Utilize Advance Machine Learning techniques and Algorithm for their Business Financial Health')
+    st.subheader('Dr Sunny is an AI Technology created for users to')
+    # st.subheader('To help quickly help their mental health using different resources. i.e Music Recommendation, Book Recommendation, and many more')
+    st.subheader('Utilize Advance Machine Learning techniques and Algorithm for Mental Health Problems')
     st.markdown('Created by: [OFIN TEAM](https://github.com/Fayaz-khani)')
     st.markdown('Contact via mail: [mudassirfayaz125@gmail.com]')
     
